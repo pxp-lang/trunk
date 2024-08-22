3b9989941da47c5857d9093a508a752c7382eb0a
Ryan Chandler <ryangjchandler@gmail.com>
2024-08-22 17:42:09 +0100

    CLI: Setup Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+use clap_verbosity_flag::Verbosity;
+
+mod cmd {
+
+}
+
+#[derive(Parser)]
+#[clap(name = "trunk", version = env!("CARGO_PKG_VERSION"), about = "The all-in-one tool for PHP development.")]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Command>,
+
+    #[clap(flatten)]
+    verbosity: Verbosity,
+}
+
+#[derive(Subcommand)]
+enum Command {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
 }
